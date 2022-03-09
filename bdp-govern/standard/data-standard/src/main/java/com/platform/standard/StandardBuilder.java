3f382824048513abea3f4675446bd34e0508e4a2
wlhbdp <wlhbdp@wlhbdpdeMacBook-Pro.local>
2022-03-10 00:17:51 +0800

    feature(master): initial data standard

---
+++ bdp-govern/standard/data-standard/src/main/java/com/platform/standard/StandardBuilder.java
@@ -0,0 +1,10 @@
+package com.platform.standard;
+
+/**
+ * build 原子指标/派生指标/衍生指标
+ */
+public class StandardBuilder {
+    public static void main(String[] args) {
+        //todo: build atomic standard
+    }
+}
