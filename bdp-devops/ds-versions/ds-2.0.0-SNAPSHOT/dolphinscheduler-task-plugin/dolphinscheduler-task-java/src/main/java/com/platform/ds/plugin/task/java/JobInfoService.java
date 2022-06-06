af08d8f7f62b5343de8900679ddce9c354f0052e
wlhbdp <leo.lh.wu@hstong.com>
2022-06-06 13:30:30 +0800

    feature(main-wlh): 定期升级维护最新的组件版本

---
+++ bdp-devops/ds-versions/ds-2.0.0-SNAPSHOT/dolphinscheduler-task-plugin/dolphinscheduler-task-java/src/main/java/com/platform/ds/plugin/task/java/JobInfoService.java
@@ -0,0 +1,17 @@
+package com.platform.ds.plugin.task.java;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @program: wlhbdp
+ * @description: 任务服务类
+ * @author: wlhbdp
+ * @create: 2021-06-16 17:29
+ **/
+public class JobInfoService {
+
+    private final static Logger logger = LoggerFactory.getLogger(JobInfoService.class);
+
+
+}
