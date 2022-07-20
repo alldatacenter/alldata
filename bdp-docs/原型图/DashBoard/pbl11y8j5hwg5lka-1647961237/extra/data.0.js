33db4f6b155f40e94a4a4e0847e9d30836e8744c
wlhbdp <wlhbdp@wlhbdpdeMacBook-Pro.local>
2022-07-20 15:33:21 +0800

    feature(branch_wlh): add 全战式AllData产品路线图v1.1-v1.4

---
+++ bdp-docs/原型图/DashBoard/pbl11y8j5hwg5lka-1647961237/extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"HhitGZQTr954c7Ug8XBvAY","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
---
+++ bdp-docs/原型图/DataGovern/pbl10d3eplvwag77-1647846222/env/2203.js
@@ -0,0 +1,1 @@
+window.ENV = {"IS_FEAT_ABOARD":true}
