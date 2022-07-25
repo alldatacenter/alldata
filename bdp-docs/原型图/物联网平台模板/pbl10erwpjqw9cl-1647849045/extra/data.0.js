05387de23bff876d4697bca9eceafb6847804d28
authorwlh <m13602574132@163.com>
2022-07-25 17:41:04 +0800

    feature(branch_wlh): rm unuse docs

--- bdp-docs/原型图/物联网平台模板/pbl10erwpjqw9cl-1647849045/extra/data.0.js
+++
@@ -1,1 +0,0 @@
-window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"Zi76AwOr934gluWEMuZJA","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
--- bdp-docs/原型图/移动端商家版APP/pbl10e07flth6aot-1647847752/extra/data.0.js
+++
@@ -1,1 +0,0 @@
-window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"sMpiNgy2r933gogoIpy26B","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
