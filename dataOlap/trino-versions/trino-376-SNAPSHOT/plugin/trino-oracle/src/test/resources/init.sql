4953ba0b474c000f1d2911e376e42400d550292f
AllDataDC <m13602574132@163.com>
2022-11-06 22:08:20 +0800

    feature(branch_wlh): Test

--- dataOlap/trino-versions/trino-376-SNAPSHOT/plugin/trino-oracle/src/test/resources/init.sql
+++
@@ -1,8 +0,0 @@
-/*
-this is added to allow more processes on database, otherwise the tests end up giving
-ORA-12519, TNS:no appropriate service handler found
-ORA-12505, TNS:listener does not currently know of SID given in connect descriptor
-to fix this we have to change the number of processes of SPFILE
-*/
-ALTER SYSTEM SET processes=1000 SCOPE=SPFILE;
-ALTER SYSTEM SET disk_asynch_io = FALSE SCOPE = SPFILE;
