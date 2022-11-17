95974306c15219a953f65716cb2968fddd80e2ee
AllDataDC <m13602574132@163.com>
2022-11-18 00:09:03 +0800

    feature(branch_wlh): 引入ModelScope

---
+++ dataAI/modelscope-versions/modelscope-master/modelscope/models/base/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from .base_head import *  # noqa F403
+from .base_model import *  # noqa F403
+from .base_torch_head import *  # noqa F403
+from .base_torch_model import *  # noqa F403
