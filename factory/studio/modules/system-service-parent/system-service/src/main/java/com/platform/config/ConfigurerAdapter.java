a81c910547b7ca2fe91488612c1ad8087a7936cf
wenyq1 <30886254+wenyq1@users.noreply.github.com>
2023-04-04 16:07:19 +0800

    Update ConfigurerAdapter.java
    修改前端和后端跨域问题，只需要在前端做跨域就可以了
    
    Signed-off-by: wenyq1 <30886254+wenyq1@users.noreply.github.com>

--- factory/studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/ConfigurerAdapter.java
+++ factory/studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/ConfigurerAdapter.java
@@ -38,12 +38,6 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 
