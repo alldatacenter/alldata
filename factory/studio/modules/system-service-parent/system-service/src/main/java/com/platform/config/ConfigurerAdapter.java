72e5732f35dd66dcfeb075da017f079865c78dff
wenyq1 <30886254+wenyq1@users.noreply.github.com>
2023-04-06 15:38:00 +0800

    Update ConfigurerAdapter.java
    双跨域问题修改
    
    Signed-off-by: wenyq1 <30886254+wenyq1@users.noreply.github.com>

--- factory/studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/ConfigurerAdapter.java
+++ factory/studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/ConfigurerAdapter.java
@@ -39,11 +39,7 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
+  
         return new CorsFilter(source);
     }
 
