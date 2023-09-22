492f61939ed5d16a1611a9c75c215b82fa868b63
yanzhenyu <yanzhenyu@huaru.cc>
2023-09-22 11:28:21 +0800

    Add WebSocket proxy configuration to the gateway.

--- studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/WebSocketConfig.java
+++ studio/modules/system-service-parent/system-service/src/main/java/com/platform/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.platform.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author AllDataDC
@@ -9,4 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebSocketConfig {
 
+    /**
+     * ServerEndpointExporter 作用
+     *
+     * 这个Bean会自动注册使用@ServerEndpoint注解声明的websocket endpoint
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
