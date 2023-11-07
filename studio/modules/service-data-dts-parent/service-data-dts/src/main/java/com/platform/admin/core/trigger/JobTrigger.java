2dc05ef6adbbae83283e06c96ec0e920ec7af9f0
AllDataDC <m13602574132@163.com>
2023-11-07 21:01:43 +0800

    [Feature][studio]thanks @jeff06143132,解决数据集成直接执行任务失败的问题,日志问题#647

--- studio/modules/service-data-dts-parent/service-data-dts/src/main/java/com/platform/admin/core/trigger/JobTrigger.java
+++ studio/modules/service-data-dts-parent/service-data-dts/src/main/java/com/platform/admin/core/trigger/JobTrigger.java
@@ -35,52 +35,53 @@ public class JobTrigger {
      *
      * @param jobId
      * @param triggerType
-     * @param failRetryCount        >=0: use this param
+     * @param failRetrypublic class JobTriggerPoolHelper {Count        >=0: use this param
      *                              <0: use param from job info config
      * @param executorShardingParam
      * @param executorParam         null: use job param
      *                              not null: cover job param
      */
     public static void trigger(int jobId, TriggerTypeEnum triggerType, int failRetryCount, String executorShardingParam, String executorParam) {
-		JobInfo jobInfo = JobAdminConfig.getAdminConfig().getJobInfoMapper().loadById(jobId);
-		if (jobInfo == null) {
-			logger.warn(">>>>>>>>>>>> trigger fail, jobId invalid，jobId={}", jobId);
-			return;
-		}
-		if (GlueTypeEnum.BEAN.getDesc().equals(jobInfo.getGlueType())) {
-			//解密账密
-			String json = JSONUtils.changeJson(jobInfo.getJobJson(), JSONUtils.decrypt);
-			jobInfo.setJobJson(json);
-		}
-		if (StringUtils.isNotBlank(executorParam)) {
-			jobInfo.setExecutorParam(executorParam);
-		}
-		int finalFailRetryCount = failRetryCount >= 0 ? failRetryCount : jobInfo.getExecutorFailRetryCount();
-		JobGroup group = JobAdminConfig.getAdminConfig().getJobGroupMapper().load(jobInfo.getJobGroup());
+        JobInfo jobInfo = JobAdminConfig.getAdminConfig().getJobInfoMapper().loadById(jobId);
+        if (jobInfo == null) {
+            logger.warn(">>>>>>>>>>>> trigger fail, jobId invalid，jobId={}", jobId);
+            return;
+        }
+        if (GlueTypeEnum.BEAN.getDesc().equals(jobInfo.getGlueType())) {
+            //解密账密
+            String json = JSONUtils.changeJson(jobInfo.getJobJson(), JSONUtils.decrypt);
+            jobInfo.setJobJson(json);
+        }
+        if (StringUtils.isNotBlank(executorParam)) {
+            jobInfo.setExecutorParam(executorParam);
+        }
+        int finalFailRetryCount = failRetryCount >= 0 ? failRetryCount : jobInfo.getExecutorFailRetryCount();
+        JobGroup group = JobAdminConfig.getAdminConfig().getJobGroupMapper().load(jobInfo.getJobGroup());
 
-		// sharding param
-		int[] shardingParam = null;
-		if (executorShardingParam != null) {
-			String[] shardingArr = executorShardingParam.split("/");
-			if (shardingArr.length == 2 && isNumeric(shardingArr[0]) && isNumeric(shardingArr[1])) {
-				shardingParam = new int[2];
-				shardingParam[0] = Integer.valueOf(shardingArr[0]);
-				shardingParam[1] = Integer.valueOf(shardingArr[1]);
-			}
-		}
-		if (ExecutorRouteStrategyEnum.SHARDING_BROADCAST == ExecutorRouteStrategyEnum.match(jobInfo.getExecutorRouteStrategy(), null)
-				&& group.getRegistryList() != null && !group.getRegistryList().isEmpty()
-				&& shardingParam == null) {
-			for (int i = 0; i < group.getRegistryList().size(); i++) {
-				processTrigger(group, jobInfo, finalFailRetryCount, triggerType, i, group.getRegistryList().size());
-			}
-		} else {
-			if (shardingParam == null) {
-				shardingParam = new int[]{0, 1};
-			}
-			System.out.println("processTrigger");
-			processTrigger(group, jobInfo, finalFailRetryCount, triggerType, shardingParam[0], shardingParam[1]);
-		}
+        // sharding param
+        int[] shardingParam = null;
+        if (executorShardingParam != null) {
+            String[] shardingArr = executorShardingParam.split("/");
+            if (shardingArr.length == 2 && isNumeric(shardingArr[0]) && isNumeric(shardingArr[1])) {
+                shardingParam = new int[2];
+                shardingParam[0] = Integer.valueOf(shardingArr[0]);
+                shardingParam[1] = Integer.valueOf(shardingArr[1]);
+            }
+        }
+        if (ExecutorRouteStrategyEnum.SHARDING_BROADCAST == ExecutorRouteStrategyEnum.match(jobInfo.getExecutorRouteStrategy(), null)
+                && group.getRegistryList() != null && !group.getRegistryList().isEmpty()
+                && shardingParam == null) {
+            logger.info("多任务processTrigger开始...");
+            for (int i = 0; i < group.getRegistryList().size(); i++) {
+                processTrigger(group, jobInfo, finalFailRetryCount, triggerType, i, group.getRegistryList().size());
+            }
+        } else {
+            if (shardingParam == null) {
+                shardingParam = new int[]{0, 1};
+            }
+            logger.info("单任务processTrigger开始...");
+            processTrigger(group, jobInfo, finalFailRetryCount, triggerType, shardingParam[0], shardingParam[1]);
+        }
 
     }
 
@@ -102,12 +103,10 @@ public class JobTrigger {
      * @param total               sharding index
      */
     private static void processTrigger(JobGroup group, JobInfo jobInfo, int finalFailRetryCount, TriggerTypeEnum triggerType, int index, int total) {
-
         TriggerParam triggerParam = new TriggerParam();
-
         // param
         ExecutorBlockStrategyEnum blockStrategy = ExecutorBlockStrategyEnum
-				.match(jobInfo.getExecutorBlockStrategy(), ExecutorBlockStrategyEnum.SERIAL_EXECUTION);  // block strategy
+                .match(jobInfo.getExecutorBlockStrategy(), ExecutorBlockStrategyEnum.SERIAL_EXECUTION);  // block strategy
         ExecutorRouteStrategyEnum executorRouteStrategyEnum = ExecutorRouteStrategyEnum.match(jobInfo.getExecutorRouteStrategy(), null);    // route strategy
         String shardingParam = (ExecutorRouteStrategyEnum.SHARDING_BROADCAST == executorRouteStrategyEnum) ? String.valueOf(index).concat("/").concat(String.valueOf(total)) : null;
 
@@ -122,8 +121,9 @@ public class JobTrigger {
         jobLog.setTriggerTime(triggerTime);
         jobLog.setJobDesc(jobInfo.getJobDesc());
 
-        JobAdminConfig.getAdminConfig().getJobLogMapper().save(jobLog);
-        logger.debug(">>>>>>>>>>> service-data-dts trigger start, jobId:{}", jobLog.getId());
+        long saveCount = JobAdminConfig.getAdminConfig().getJobLogMapper().save(jobLog);
+        logger.info(">>>>>>>>>>> service-data-dts trigger start, jobId:{}", jobLog.getId());
+        logger.info(">>>>>>>>>>> service-data-dts trigger start, saveCount:{}", saveCount);
 
         // 2、init trigger-param
         triggerParam.setJobId(jobInfo.getId());
@@ -164,51 +164,52 @@ public class JobTrigger {
         // 3、init address
         String address = null;
         ReturnT<String> routeAddressResult = null;
-//        if (group.getRegistryList() != null && !group.getRegistryList().isEmpty()) {
-//            if (ExecutorRouteStrategyEnum.SHARDING_BROADCAST == executorRouteStrategyEnum) {
-//                if (index < group.getRegistryList().size()) {
-//                    address = group.getRegistryList().get(index);
-//                } else {
-//                    address = group.getRegistryList().get(0);
-//                }
-//            } else {
-//                routeAddressResult = executorRouteStrategyEnum.getRouter().route(triggerParam, group.getRegistryList());
-//                if (routeAddressResult.getCode() == ReturnT.SUCCESS_CODE) {
-//                    address = routeAddressResult.getContent();
-//                }
-//            }
-//        } else {
-//            routeAddressResult = new ReturnT<String>(ReturnT.FAIL_CODE, I18nUtil.getString("jobconf_trigger_address_empty"));
-//        }
+        if (group.getRegistryList() != null && !group.getRegistryList().isEmpty()) {
+            if (ExecutorRouteStrategyEnum.SHARDING_BROADCAST == executorRouteStrategyEnum) {
+                if (index < group.getRegistryList().size()) {
+                    address = group.getRegistryList().get(index);
+                } else {
+                    address = group.getRegistryList().get(0);
+                }
+            } else {
+                routeAddressResult = executorRouteStrategyEnum.getRouter().route(triggerParam, group.getRegistryList());
+                if (routeAddressResult.getCode() == ReturnT.SUCCESS_CODE) {
+                    address = routeAddressResult.getContent();
+                }
+            }
+        } else {
+            routeAddressResult = new ReturnT<String>(ReturnT.FAIL_CODE, I18nUtil.getString("jobconf_trigger_address_empty"));
+        }
 
-		// 4、trigger remote executor
-		ReturnT<String> triggerResult = null;
-		if (address != null) {
-			triggerResult = runExecutor(triggerParam, address);
-		} else {
-			triggerResult = new ReturnT<String>(ReturnT.FAIL_CODE, null);
-		}
+        // 4、trigger remote executor
+        ReturnT<String> triggerResult = null;
+        if (address != null) {
+            triggerResult = runExecutor(triggerParam, address);
+        } else {
+            triggerResult = new ReturnT<String>(ReturnT.FAIL_CODE, null);
+        }
 
-		// 5、collection trigger info
-		StringBuffer triggerMsgSb = new StringBuffer();
-		triggerMsgSb.append(I18nUtil.getString("jobconf_trigger_type")).append("：").append(triggerType.getTitle());
-		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_admin_adress")).append("：").append(
-				IpUtil.getIp());
-//		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_exe_regtype")).append("：")
-//				.append((group.getAddressType() == 0) ? I18nUtil.getString("jobgroup_field_addressType_0") : I18nUtil.getString("jobgroup_field_addressType_1"));
-//		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_exe_regaddress")).append("：").append(group.getRegistryList());
-		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorRouteStrategy")).append("：").append(executorRouteStrategyEnum.getTitle());
-		if (shardingParam != null) {
-			triggerMsgSb.append("(" + shardingParam + ")");
-		}
-		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorBlockStrategy")).append("：").append(blockStrategy.getTitle());
-		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_timeout")).append("：").append(jobInfo.getExecutorTimeout());
-		triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorFailRetryCount")).append("：").append(finalFailRetryCount);
+        // 5、collection trigger info
+        StringBuffer triggerMsgSb = new StringBuffer();
+        triggerMsgSb.append(I18nUtil.getString("jobconf_trigger_type")).append("：").append(triggerType.getTitle());
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_admin_adress")).append("：").append(
+                IpUtil.getIp());
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_exe_regtype")).append("：")
+                .append((group.getAddressType() == 0) ? I18nUtil.getString("jobgroup_field_addressType_0") : I18nUtil.getString("jobgroup_field_addressType_1"));
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobconf_trigger_exe_regaddress")).append("：").append(group.getRegistryList());
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorRouteStrategy")).append("：").append(executorRouteStrategyEnum.getTitle());
+        if (shardingParam != null) {
+            triggerMsgSb.append("(" + shardingParam + ")");
+        }
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorBlockStrategy")).append("：").append(blockStrategy.getTitle());
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_timeout")).append("：").append(jobInfo.getExecutorTimeout());
+        triggerMsgSb.append("<br>").append(I18nUtil.getString("jobinfo_field_executorFailRetryCount")).append("：").append(finalFailRetryCount);
 
-		triggerMsgSb.append("<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>" + I18nUtil.getString("jobconf_trigger_run") + "<<<<<<<<<<< </span><br>")
-				.append((routeAddressResult != null && routeAddressResult.getMsg() != null) ? routeAddressResult.getMsg() + "<br><br>" : "").append(triggerResult.getMsg() != null ? triggerResult.getMsg() : "");
 
-		// 6、save log trigger-info
+        triggerMsgSb.append("<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>" + I18nUtil.getString("jobconf_trigger_run") + "<<<<<<<<<<< </span><br>")
+                .append((routeAddressResult != null && routeAddressResult.getMsg() != null) ? routeAddressResult.getMsg() + "<br><br>" : "").append((triggerResult != null && triggerResult.getMsg() != null) ? triggerResult.getMsg() : "");
+
+        // 6、save log trigger-info
         jobLog.setExecutorAddress(address);
         jobLog.setExecutorHandler(jobInfo.getExecutorHandler());
         jobLog.setExecutorParam(jobInfo.getExecutorParam());
@@ -216,9 +217,10 @@ public class JobTrigger {
         jobLog.setExecutorFailRetryCount(finalFailRetryCount);
         jobLog.setTriggerCode(triggerResult.getCode());
         jobLog.setTriggerMsg(triggerMsgSb.toString());
-        JobAdminConfig.getAdminConfig().getJobLogMapper().updateTriggerInfo(jobLog);
+        int uodateCount = JobAdminConfig.getAdminConfig().getJobLogMapper().updateTriggerInfo(jobLog);
 
-        logger.debug(">>>>>>>>>>> service-data-dts trigger end, jobId:{}", jobLog.getId());
+        logger.info(">>>>>>>>>>> service-data-dts trigger end, jobId:{}", jobLog.getId());
+        logger.info(">>>>>>>>>>> service-data-dts trigger end, uodateCount:{}", uodateCount);
     }
 
     private static long getMaxId(JobInfo jobInfo) {
@@ -239,7 +241,7 @@ public class JobTrigger {
         try {
             // 进行任务的触发
             ExecutorBiz executorBiz = new ExecutorBizImpl();
-            executorBiz.run(triggerParam);
+            runResult = executorBiz.run(triggerParam);
         } catch (Exception e) {
             logger.error(">>>>>>>>>>> service-data-dts trigger error, please check if the executor[{}] is running.", address, e);
             runResult = new ReturnT<String>(ReturnT.FAIL_CODE, ThrowableUtil.stackTraceToString(e));
@@ -247,10 +249,9 @@ public class JobTrigger {
 
         StringBuffer runResultSB = new StringBuffer(I18nUtil.getString("jobconf_trigger_run") + "：");
         runResultSB.append("<br>address：").append(address);
-//        runResultSB.append("<br>code：").append(runResult.getCode());
-//        runResultSB.append("<br>msg：").append(runResult.getMsg());
-
-//        runResult.setMsg(runResultSB.toString());
+        runResultSB.append("<br>code：").append(runResult.getCode());
+        runResultSB.append("<br>msg：").append(runResult.getMsg());
+        runResult.setMsg(runResultSB.toString());
         return runResult;
     }
 
