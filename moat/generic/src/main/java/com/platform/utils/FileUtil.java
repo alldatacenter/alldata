6295510330eceec64a071ca95f8e870309964917
alldatafounder <m13602574132@163.com>
2025-05-22 18:07:33 +0800

    [Feature][AllData]xlsx file export unused code remove

--- moat/generic/src/main/java/com/platform/utils/FileUtil.java
+++ moat/generic/src/main/java/com/platform/utils/FileUtil.java
@@ -25,8 +25,8 @@ import java.util.stream.Collectors;
 /**
  * File工具类，扩展 hutool 工具包
  *
- * @author Zheng Jie
- * @date 2018-12-27
+ * @author AllDataDC
+ * @date 2023-01-27
  */
 @Slf4j
 public class FileUtil extends cn.hutool.core.io.FileUtil {
@@ -195,30 +195,6 @@ public class FileUtil extends cn.hutool.core.io.FileUtil {
      * 导出excel
      */
     public static void downloadExcel(List<Map<String, Object>> list, HttpServletResponse response) throws IOException {
-//        String tempPath = SYS_TEM_DIR + IdUtil.fastSimpleUUID() + ".xlsx";
-//        File file = new File(tempPath);
-//        BigExcelWriter writer = ExcelUtil.getBigWriter(file);
-//        // 一次性写出内容，使用默认样式，强制输出标题
-//        writer.write(list, true);
-//        SXSSFSheet sheet = (SXSSFSheet)writer.getSheet();
-//        //上面需要强转SXSSFSheet  不然没有trackAllColumnsForAutoSizing方法
-//        sheet.trackAllColumnsForAutoSizing();
-//        //列宽自适应
-//        writer.autoSizeColumnAll();
-//        //response为HttpServletResponse对象
-//        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
-//        //test.xls是弹出下载对话框的文件名，不能为中文，中文请自行编码
-//        response.setHeader("Content-Disposition", "attachment;filename=file.xlsx");
-//        ServletOutputStream out = response.getOutputStream();
-//        // 终止后删除临时文件
-//        file.deleteOnExit();
-//        writer.flush(out, true);
-//        //此处记得关闭输出Servlet流
-//        IoUtil.close(out);
-
-
-        //---------------------------------------------------------------//
-
         String tempPath = SYS_TEM_DIR + IdUtil.fastSimpleUUID() + ".xlsx";
         File file = new File(tempPath);
         ExcelWriter writer = ExcelUtil.getBigWriter(file);
